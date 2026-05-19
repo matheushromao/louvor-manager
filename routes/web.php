6ba871107ab0f9384de29ff56d2c0ad68957a7da
Matheus Romão <matheus_henrique_9@hotmail.com>
2026-05-19 18:52:28 -0300

    add usuários comuns para ver o repertório

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,7 @@ Route::get('/', function () {
 Route::middleware('auth')->group(function () {
     Route::get('categorias', [CategoriaController::class, 'index']) -> name('categorias.index');
     Route::get('musicas', [MusicaController::class, 'index']) -> name('musicas.index');
+    Route::get('repertorios', [RepertorioController::class, 'index']) -> name('repertorios.index');
 });
 
 // Rotas protegidas por autenticação e autorização para administradores
